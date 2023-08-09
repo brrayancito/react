e9650b4163f753627183907338f08ba605fc07e2
Brrayan Javy <brrayanperez10@gmail.com>
2023-08-09 19:09:19 -0400

    Added filter by category and price

--- projects/06-shopping-cart/src/components/Filters.jsx
+++ projects/06-shopping-cart/src/components/Filters.jsx
@@ -1,14 +1,44 @@
-export function Filters () {
+import { useState } from 'react'
+import './Filters.css'
+
+export function Filters ({ onChangeFilters }) {
+  const [minPrice, setMinPrice] = useState(0)
+
+  const handleChangeMinPrice = (event) => {
+    setMinPrice(event.target.value)
+    onChangeFilters(prevState => ({
+      ...prevState,
+      minPrice: event.target.value
+    }))
+  }
+
+  const handleChangeCategory = (event) => {
+    onChangeFilters(prevState => ({
+      ...prevState,
+      category: event.target.value
+    }))
+  }
+
   return (
         <section className="filters">
 
             <div>
-                <label htmlFor="price">Price</label>
+                <label htmlFor="price">Min price</label>
                 <input
                      type="range"
                      id="price"
                      min='0'
-                     max='1000' />
+                     max='1000'
+                     onChange={handleChangeMinPrice} />
+                    <span>${minPrice}</span>
+            </div>
+            <div>
+              <label htmlFor="category">Category</label>
+              <select id="category" onChange={handleChangeCategory}>
+                <option value="all">All</option>
+                <option value="laptops">Laptops</option>
+                <option value="smartphones">Smartphones</option>
+              </select>
             </div>
         </section>
   )
