85f44b9b459e0e3e3cd86e31ebe16a5f3cdb9e21
Brrayan Javy <brrayanperez10@gmail.com>
2023-08-12 20:36:51 -0400

    using useContext to add a global state

---
+++ projects/06-shopping-cart/src/context/filters.jsx
@@ -0,0 +1,19 @@
+import { createContext, useState } from 'react'
+
+// 1) Create the context
+export const FiltersContext = createContext()
+
+// 2) Create the provider, to provide the context to the app
+export function FiltersProvider ({ children }) {
+  const [filters, setFilters] = useState({
+    category: 'all',
+    minPrice: 0
+  })
+  return (
+        <FiltersContext.Provider value={{
+          filters, setFilters
+        }}>
+            {children}
+        </FiltersContext.Provider>
+  )
+}
